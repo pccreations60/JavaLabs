156c7f558763b667db94cbb039643fd7eba0e3fe
Paul Childs <pccreations.paul@gmail.com>
2023-10-03 15:11:45 -0400

    fixed

--- FirstDemoLab/src/FirstDemoLlab/FirstDemoLab.java
+++ FirstDemoLab/src/FirstDemoLlab/FirstDemoLab.java
@@ -1,9 +1,10 @@
+package FirstDemoLlab;
 
 public class FirstDemoLab {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Thar")
+		System.out.println("Hello Thar");
 	}
 
 }
