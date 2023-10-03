7358519d50e05835ad05add1dd46611b81da046e
Paul Childs <pccreations.paul@gmail.com>
2023-10-03 17:59:25 -0400

    Second commit

--- FirstDemoLab/src/FirstDemoLlab/FirstDemoLab.java
+++ FirstDemoLab/src/FirstDemoLlab/FirstDemoLab.java
@@ -5,6 +5,7 @@ public class FirstDemoLab {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Thar");
+		System.out.println("You Rascal");
 	}
 
 }
